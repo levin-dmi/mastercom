94213b92e96bebd478435bdea4f68a27cb061fa5
levin-dmi <levin.dmi@gmail.com>
2023-08-31 19:46:10 +0300

    Проверка форм и javascriptинг

--- incoming/forms.py
+++ incoming/forms.py
@@ -1,5 +1,6 @@
-from django.forms import ModelForm, Form, ChoiceField, Select
+from django.forms import ModelForm, Form, ChoiceField, Select, ValidationError
 from .models import *
+from decimal import Decimal
 
 
 class ProjectForm(ModelForm):
@@ -14,17 +15,58 @@ class ContractForm(ModelForm):
         fields = ('number', 'date', 'name', 'description', 'project', 'total_sum', 'material_sum', 'work_sum',
                   'prepaid', 'prepaid_close_method', 'retention_percent', 'status')
 
+    def clean_total_sum(self):
+        total_sum = (self.cleaned_data['total_sum'] or 0)
+
+        # Проверяем
+        if total_sum < Decimal(self.data['material_sum'] or 0) + Decimal(self.data['work_sum'] or 0):
+            raise ValidationError('Общая сумма не может быть меньше сумм материалов и работ')
+
+        return self.cleaned_data['total_sum']
+
+    def clean_prepaid(self):
+        prepaid_sum = (self.cleaned_data['prepaid'] or 0)
+
+        # Проверяем
+        if prepaid_sum > Decimal(self.data['total_sum'] or 0):
+            raise ValidationError('Сумма аванса не может быть больше общей суммы')
+
+        return self.cleaned_data['prepaid']
 
 class ActForm(ModelForm):
     class Meta:
         model = Act
         fields = ('number', 'date', 'contract', 'total_sum', 'material_sum', 'work_sum', 'status')
 
+    def clean_total_sum(self):
+        total_sum = (self.cleaned_data['total_sum'] or 0)
+
+        # Проверяем
+        if total_sum < Decimal(self.data['material_sum'] or 0) + Decimal(self.data['work_sum'] or 0):
+            raise ValidationError('Общая сумма не может быть меньше сумм материалов и работ')
+
+        return self.cleaned_data['total_sum']
 
 class PaymentForm(ModelForm):
     class Meta:
         model = Payment
         fields = ('number', 'date', 'contract', 'total_sum', 'prepaid_sum', 'retention_sum', 'status')
+
+    def clean_total_sum(self):
+        total_sum = self.cleaned_data['total_sum']
+
+        # Проверяем
+        if total_sum < Decimal(self.data['prepaid_sum'] or 0) + Decimal(self.data['retention_sum'] or 0):
+            raise ValidationError('Общая сумма не может быть меньше сумм аванса и возврата удержаний')
+
+        return total_sum
+
+    def clean_prepaid_sum(self):
+        return self.cleaned_data['total_sum'] or 0
+
+    def clean_retention_sum(self):
+        return self.cleaned_data['retention_sum'] or 0
+
 
 class ListFilterForm(Form):
     projects = [['', 'ВСЕ'],] + [[str(obj.pk), obj] for obj in Project.objects.all()]
